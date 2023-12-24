492bc79795da8b4d26c921653ab4a25517566f6f
Samir Akhmed <warrior.owl@yandex.ru>
2023-12-24 13:52:24 +0300

    Finalize untested file api

--- file_service/src/core/config.py
+++ file_service/src/core/config.py
@@ -1,7 +1,7 @@
 import os.path
 from logging import config as logging_config
 
-from pydantic import Field, PostgresDsn, RedisDsn
+from pydantic import Field, PostgresDsn
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from src.core.logger import LOGGING
 
@@ -18,10 +18,14 @@ class Settings(BaseSettings):
         env="DESCRIPTION",
     )
     version: str = Field("1.0.0", alias="VERSION", env="VERSION")
-    redis_conn: RedisDsn = Field(..., alias="REDIS_CONN", env="REDIS_CONN")
+    redis_host: str = Field(..., alias="REDIS_HOST", env="REDIS_HOST")
+    redis_port: int = Field(..., alias="REDIS_PORT", env="REDIS_PORT")
     base_dir: str = os.path.dirname(os.path.abspath(__file__))
     postgres_conn: PostgresDsn = Field(..., alias="POSTGRES_CONN", env="POSTGRES_CONN")
     s3_bucket: str = Field(..., alias="S3_BUCKET", env="S3_BUCKET")
+    endpoint: str = Field(..., alias="MINIO_ENDPOINT", env="MINIO_ENDPOINT")
+    access_key: str = Field(..., alias="MINIO_ACCESS_KEY", env="MINIO_ACCESS_KEY")
+    secret_key: str = Field(..., alias="MINIO_SECRET_KEY", env="MINIO_SECRET_KEY")
 
 
 settings = Settings()
